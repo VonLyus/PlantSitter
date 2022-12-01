eb0e9717bca884b4d41bf3d56c1f24428c8ac83a
sid030119 <sid030119@naver.com>
2022-12-01 18:25:27 +0900

    통합중2

---
+++ Assets/name.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class name : MonoBehaviour
+{
+    public string plant;
+   public void Cactus_detect()
+    {
+        plant = "Cactus";
+    }
+    public void camellia_detect()
+    {
+        plant = "camellia";
+    }
+    public void moneytree_detect()
+    {
+        plant = "moneytree";
+    }
+
+}
